c6bb35d725ca19b16a8b8584c2cc559c94799b48
dong-onion <>
2025-07-22 12:33:15 +0900

    reanimated, gesture handler 설치

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: ['react-native-reanimated/plugin'], // 이 줄 꼭 마지막에!
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RootStackNavigator from './navigators/RootStackNavigator';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
-  return <RootStackNavigator />;
+  return (
+    <GestureHandlerRootView>
+      <RootStackNavigator />
+    </GestureHandlerRootView>
+  );
 }
 
 const styles = StyleSheet.create({
